685cbc556734750842fc49ba2de684977c492fd0
Tim Ritter <timrittermail@gmail.com>
2022-04-16 16:25:27 +0000

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Tim Ritter</title>
+        <meta name="description" content="Tim Ritters personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-black w-screen h-screen">
+       
+      </main>
+
+      <footer>
+       
+      </footer>
+    </>
+  )
+}
