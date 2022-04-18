7ee99026bb39410087f720f20d09de45f0f310f8
Tim Ritter <timrittermail@gmail.com>
2022-04-18 21:31:37 +0000

    Reduce redundancy and improve separation of concerns

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
+import DarkModeDetector from "../components/utility/dark-mode-detector";
+import Head from 'next/head';
 import '../styles/globals.scss'
-import DarkModeDetector from "../components/dark-mode-detector";
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <div className='bg-gray-50 dark:bg-zinc-900 min-h-screen flex flex-col justify-between'>
+      <Head>
+        <title>Tim Ritter</title>
+        <meta name="description" content="Tim Ritter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <DarkModeDetector />
       <Component {...pageProps} />
-    </>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,25 @@
-import Head from 'next/head'
 import FadeIn from 'react-fade-in'
 import Footer from '../components/layout/footer'
 import Project from '../components/projects/project'
+import PageSection from '../components/layout/page-section'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Tim Ritter</title>
-        <meta name="description" content="Tim Ritter - personal website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <div className="container mx-auto py-10 sm:py-20 lg:py-32 xl:py-44 px-10 sm:px-20 overflow-hidden">
           <FadeIn>
-            <div className="page-section">
+            <PageSection>
               <p className="lead mb-4 md:mb-5">
                 Tim Ritter
               </p>
               <p>Creating <em>exciting</em>, <em>inclusive</em>, and <em>accessible</em> user experiences on the world wide web.</p>
-            </div>
-            <div className="page-section">
+            </PageSection>
+            <PageSection>
               <p className='mb-3 secondary'><small>About Me</small></p>
               <p>Hey there! I&apos;m <em>Tim</em>, a {(Math.floor((Number(new Date()) - new Date('2004-06-05').getTime()) / 3.15576e+10)).toString()}-year-old trainee Software Developer from Germany. I&apos;ve been interested in visually pleasing web experiences for a while. One day I started out creating websites as a hobby. Now I do it full-time and try to gradually improve my work one step at a time.</p>
-            </div>
-            <div className="page-section">
+            </PageSection>
+            <PageSection>
               <div className="py-2 md:py-3">
                 <p className='mb-3 secondary'><small>Projects</small></p>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-5">
@@ -51,11 +45,11 @@ export default function Home() {
                   />
                 </div>
               </div>
-            </div>
-            <div className="page-section">
+            </PageSection>
+            <PageSection>
               <p className='mb-3 secondary'><small>Contact</small></p>
               <p>Let&apos;s get in <em>touch</em>. Contact me at <a href="mailto:t-ritter-mail@web.de">t-ritter-mail@web.de</a></p>
-            </div>
+            </PageSection>
           </FadeIn>
         </div>
       </main>
