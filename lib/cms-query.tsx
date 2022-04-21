8e2a30ef6619c2fdc7ed4a400f71537e86f0bb17
Tim Ritter <timrittermail@gmail.com>
2022-04-21 16:59:13 +0000

    Add query function to reduce redundancy when queriying data from DatoCMS

---
+++ lib/cms-query.tsx
@@ -0,0 +1,20 @@
+export default async function query(query: string) {
+    const token = process.env.DATO_API_KEY
+    const response = await fetch(
+        'https://graphql.datocms.com/',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                query: `${query}`
+            }),
+        }
+    )
+    const responseData = (await response.json())?.data;
+
+    return responseData;
+}
