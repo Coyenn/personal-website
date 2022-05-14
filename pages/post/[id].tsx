e125e48310eca16e3b75652f6684ad3ff2c83930
Tim Ritter <timrittermail@gmail.com>
2022-05-14 19:06:03 +0000

    Restyle blog post site

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import FadeIn from "react-fade-in/lib/FadeIn";
+import Image from "next/image";
 import PageSection from "../../components/layout/page-section";
 import { ArrowLeftIcon } from "@heroicons/react/solid";
 import ReactMarkdown from "react-markdown";
 import query from "../../lib/cms-query";
 import React from "react";
+import Container from "../../components/layout/container";
 
 interface BlogPostProps {
     id: string;
     token: string;
     post: Post;
+}
+
+interface PostThumbnail {
+    url: string;
+    alt: string;
 }
 
 interface Post {
@@ -19,6 +25,7 @@ interface Post {
     published: boolean;
     _status: string;
     _firstPublishedAt: string;
+    thumbnail: PostThumbnail;
 }
 interface BlogPostState {
     loading: boolean;
@@ -50,11 +57,14 @@ class PostView extends React.Component<BlogPostProps, BlogPostState> {
             <>
                 <main>
                     <article>
-                        <FadeIn>
-                            <PageSection>
+                        <PageSection
+                            size="2xl"
+                            className="border-b border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-900"
+                        >
+                            <Container>
                                 <div className="mb-10">
                                     <Link href="/" passHref={true}>
-                                        <a className="link cursor-pointer">
+                                        <a className="cursor-pointer text-black dark:text-white">
                                             <ArrowLeftIcon className="mr-2 inline h-4 w-4" />
                                             Go back
                                         </a>
@@ -62,12 +72,25 @@ class PostView extends React.Component<BlogPostProps, BlogPostState> {
                                 </div>
                                 {this.props.post?.title !== undefined &&
                                 this.props.post?.published === true ? (
-                                    <FadeIn>
-                                        <div className="flex flex-col justify-between md:flex-row">
-                                            <h1 className="lead mb-4 font-normal md:mb-5">
-                                                {this.props.post?.title}
-                                            </h1>
-                                            <p className="order-first mb-2 text-neutral-400 md:order-last md:mb-0">
+                                    <div className="flex flex-col gap-5">
+                                        <Image
+                                            className="w-full rounded-lg"
+                                            src={
+                                                this.props.post?.thumbnail?.url
+                                            }
+                                            alt={
+                                                this.props.post?.thumbnail?.alt
+                                            }
+                                            width={1920}
+                                            height={1080}
+                                            quality={50}
+                                            blurDataURL={
+                                                this.props.post?.thumbnail?.url
+                                            }
+                                            placeholder="blur"
+                                        />
+                                        <div className="flex flex-col gap-5 md:mx-20 lg:mx-32">
+                                            <p className="text-neutral-400">
                                                 <small>
                                                     <span className="sr-only">
                                                         This blog post was first
@@ -80,18 +103,21 @@ class PostView extends React.Component<BlogPostProps, BlogPostState> {
                                                     }
                                                 </small>
                                             </p>
+                                            <h1 className="text-2xl text-black dark:text-white md:text-3xl lg:text-5xl">
+                                                {this.props.post?.title}
+                                            </h1>
+                                            <div className="prose min-w-full dark:prose-invert md:text-lg">
+                                                <ReactMarkdown>
+                                                    {this.props.post?.content}
+                                                </ReactMarkdown>
+                                            </div>
                                         </div>
-                                        <div className="prose prose-invert">
-                                            <ReactMarkdown>
-                                                {this.props.post?.content}
-                                            </ReactMarkdown>
-                                        </div>
-                                    </FadeIn>
+                                    </div>
                                 ) : (
                                     <></>
                                 )}
-                            </PageSection>
-                        </FadeIn>
+                            </Container>
+                        </PageSection>
                     </article>
                 </main>
             </>
@@ -107,7 +133,7 @@ export async function getStaticProps(context) {
     const token = process.env.DATO_API_KEY;
     const post = (
         await query(
-            `{blogPost(filter: {id: {eq: "${id}"}}) { id, title, content, published, _status, _firstPublishedAt }}`,
+            `{blogPost(filter: {id: {eq: "${id}"}}) { id, thumbnail { url, alt }, title, content, published, _status, _firstPublishedAt }}`,
             token
         )
     )?.blogPost;
