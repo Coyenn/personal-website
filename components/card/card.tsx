880dab531b7ee97ce7e774267746bf4e11f8013d
Tim <61044138+Coyenn@users.noreply.github.com>
2022-05-21 20:43:32 +0000

    Fix card

---
+++ components/card/card.tsx
@@ -0,0 +1,25 @@
+interface ButtonProps {
+    children: React.ReactNode;
+    link?: string;
+}
+
+export default function Card(props: ButtonProps) {
+    return (
+        <>
+            {props.link !== undefined ? (
+                <a
+                    href={props.link}
+                    target={"_blank"}
+                    rel="noreferrer"
+                    className="flex flex-col gap-5 rounded-lg border border-neutral-200 bg-white p-5 no-underline hover:border-neutral-300 dark:border-neutral-800 dark:bg-neutral-900 dark:hover:border-neutral-600 lg:gap-8 lg:p-8"
+                >
+                    {props.children}
+                </a>
+            ) : (
+                <div className="flex flex-col gap-5 rounded-lg border border-neutral-200 bg-white p-5 no-underline dark:border-neutral-800 dark:bg-neutral-900 lg:gap-8 lg:p-8">
+                    {props.children}
+                </div>
+            )}
+        </>
+    );
+}
